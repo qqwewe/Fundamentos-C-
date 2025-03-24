628414fab7b1318c62defac036526ece24f38fc0
Renato Campos Viana <renacv@MacBook-Air-de-Renato.local>
2025-03-24 08:26:19 -0300

    feat: runner com os exercicios

--- TP2/runner.cs
+++ TP2/runner.cs
@@ -4,8 +4,17 @@ public class runner
 {
     public static void Main(string[] args)
     {
-        // TP2.EX01();
-        
+        TP2.EX01();
         TP2.EX02();
+        TP2.EX03();
+        TP2.EX04();
+        TP2.EX05();
+        TP2.EX06();
+        TP2.EX07();
+        TP2.EX08();
+        TP2.EX09();
+        TP2.EX10();
+        TP2.EX11();
+        TP2.EX12();
     }
 }
